b8ac3735b011f79b34f30e76b50bc68ab6ff0e36
Lívia <pereira.livia@gmail.com>
2024-06-10 23:47:53 -0300

    Fix metadata icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,7 @@ export const metadata: Metadata = {
     "Análise online",
   ],
   openGraph: {
-    title:
-      "Gabriela Romanhole - Atendimento psicoterapêutico individual online",
+    title: "Gabriela Romanhole - Atendimento psicoterapêutico online",
     description:
       "Atendimento psicoterapêutico individual online com abordagem psicanalítica.",
     type: "website",
@@ -36,13 +35,27 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title:
-      "Gabriela Romanhole - Atendimento psicoterapêutico individual online",
+    title: "Gabriela Romanhole - Atendimento psicoterapêutico online",
     description:
       "Atendimento psicoterapêutico individual online com abordagem psicanalítica.",
     images: [
       "https://pereiralivia.github.io/gabrielaromanhole/gabriela-romanhole.webp",
     ],
+  },
+  alternates: {
+    canonical: "https://pereiralivia.github.io/gabrielaromanhole/",
+    languages: {
+      "pt-BR": "https://pereiralivia.github.io/gabrielaromanhole/",
+    },
+  },
+  icons: {
+    icon: "https://pereiralivia.github.io/gabrielaromanhole/icon.png",
+    shortcut: "https://pereiralivia.github.io/gabrielaromanhole/icon.png",
+    apple: "https://pereiralivia.github.io/gabrielaromanhole/icon.png",
+    other: {
+      rel: "apple-touch-icon-precomposed",
+      url: "https://pereiralivia.github.io/gabrielaromanhole/icon.png",
+    },
   },
 };
 
--- app/sections/heading.tsx
+++ app/sections/heading.tsx
@@ -3,10 +3,10 @@ import { playfairDisplay } from "../ui/fonts";
 export const Heading = () => {
   return (
     <div className={`pt-16 px-4 md:px-20 ${playfairDisplay.className}`}>
-      <div className="uppercase text-5xl font-bold">
+      <h1 className="uppercase text-5xl font-bold">
         <span className="text-[#323336]">Gabriela </span>
         <span className="text-[#9d6d49]">Romanhole</span>
-      </div>
+      </h1>
       <div className="pt-1 text-lg">Psicóloga & Psicanalista</div>
     </div>
   );
